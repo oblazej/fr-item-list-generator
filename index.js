d4e7048dcc6b1a0ec2cff658786703af6059cfe1
Blaze <blazej.opyrchal@gmail.com>
2022-07-10 23:57:26 +0200

    create basic css

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const input = document.getElementById("user-input");
 const output = document.getElementById("user-output");
 const items = document.getElementById("user-items");
 
-button.onclick = function () {
+button.onclick = function (e) {
     const template = document.createElement("template");
     let list = [];
     template.innerHTML = input.value;
+    input.value = "";
 
     for (let i = 0; i < template.content.firstChild.childNodes.length; i++) {
         if (i % 2 === 1) {
             if (template.content.firstChild.childNodes[i].childNodes[1].childNodes[1].childNodes[1] !== undefined) {
                 list.push({
                     name: template.content.firstChild.childNodes[i].childNodes[1].childNodes[1].childNodes[1].getAttribute("data-name"),
-                    quantity: template.content.firstChild.childNodes[i].childNodes[1].childNodes[3].textContent.trim()
+                    // quantity: template.content.firstChild.childNodes[i].childNodes[1].childNodes[3].textContent.trim()
                 }
                 )
             } else {
                 list.push({name: template.content.firstChild.childNodes[i].childNodes[1].childNodes[1].getAttribute("data-name"),
-            quantity: template.content.firstChild.childNodes[i].childNodes[1].childNodes[3].childNodes[3].value
+            // quantity: template.content.firstChild.childNodes[i].childNodes[1].childNodes[3].childNodes[3].value
                 }
                     )
             }
@@ -28,7 +29,7 @@ button.onclick = function () {
     console.log(list)
 
     list.forEach(function(item){
-        output.innerHTML += `${item.name} x${item.quantity.replace("*", "")}, `
-        items.innerHTML += `[item=${item.name}]`
+        output.textContent += `${item.name}, `
+        items.textContent += `[item=${item.name}]`
     })
 }
